d98fea26fcbefe9510e7a04e91aa96cb0d3238bb
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2015-10-20 18:00:11 +0100

    Main file

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.EventQueue;
+
+
+/**
+ * Created by Aaron on 20/10/2015.
+ */
+
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main ex = new Main();
+                ex.setVisible(true);
+            }
+        });
+    }
+
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setSize(1366, 768);
+
+        setTitle("Game :)");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+
+}
