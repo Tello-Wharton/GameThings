10f4efd39581165f8bcd5a94d463cb42fc35247e
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2015-10-21 01:30:43 +0100

    Player body and arms move according to WASD

--- src/Arm.java
+++ src/Arm.java
@@ -11,25 +11,67 @@ import java.io.IOException;
 public class Arm extends Entity {
 
     private BufferedImage body;
-    AffineTransform at;
+    //private AffineTransform at;
 
-    public Arm(String image) {
+    private double b;
+
+    private static Player player;
+    private static double bStart = 0;
+
+    private int xPos;
+    private int yPos;
+
+    private double rotationConstant = 0.1;
+
+    public Arm(String image, Player p) {
         super(image);
+        player = p;
+
+        //at = new AffineTransform();
+        //at.translate(player.centerX(), player.centerY() - 80);
+
         try {
             body = ImageIO.read(new File(image));
         }catch (IOException e) {
 
         }
+        b = bStart;
 
-        at = new AffineTransform();
+
+        bStart += Math.toRadians(36);
+
+
+
+
     }
+
 
     @Override
     public void update() {
-        at.rotate(0.1);
+
+        //at.rotate(0.01, 0, 80);
+
+
+        //at.rotate(0.1);
+
+
+
+
     }
     public void draw(Graphics g){
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(body, at, null);
+        Graphics2D g2d = (Graphics2D) g;//.create();
+        g2d.drawImage(body, at(), board);
+        //g.dispose();
+
     }
+
+    private int a = 0;
+    private AffineTransform at(){
+        AffineTransform at = new AffineTransform();
+        at.translate(player.centerX(), player.centerY() - 80);
+        at.rotate(b + 0.01 * a, 0, 80);
+        a+=1;
+        return at;
+    }
+
 }
